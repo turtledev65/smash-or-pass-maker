cb46d28e51fd31e4568cc759afca287b308b6015
turtledev <turtledev65@gmail.com>
2023-12-15 23:09:20 +0200

    Remove positioning from the share component and add go back link

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,6 +2,8 @@ import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import ImageCarousel from "./image-carousel";
 import ShareWithFriends from "./share";
+import { TiArrowBack as BackIcon } from "react-icons/ti";
+import Link from "next/link";
 
 const SmashOrPassPage = async ({ params }: { params: { id: string } }) => {
   const list = await prisma.smashOrPassList.findUnique({
@@ -10,8 +12,14 @@ const SmashOrPassPage = async ({ params }: { params: { id: string } }) => {
   if (!list) notFound();
 
   return (
-    <div className="h-full w-full">
-      <ShareWithFriends />
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="space-y-3 text-white">
+        <ShareWithFriends />
+        <Link href="/" className="flex gap-1 text-xl">
+          <BackIcon className="h-7 w-7" />
+          Go back
+        </Link>
+      </div>
       <ImageCarousel images={list.images} />
     </div>
   );
--- app/[id]/share.tsx
+++ app/[id]/share.tsx
@@ -2,7 +2,7 @@ import ClipboardInput from "./clipboard-input";
 
 const ShareWithFriends = () => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 space-y-3 text-white">
+    <div>
       <h2 className="text-3xl">Share with your friends</h2>
       <ClipboardInput />
     </div>
