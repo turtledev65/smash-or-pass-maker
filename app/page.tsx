fcd7cd432d3c137b8125e7ad07c500b3bbe73db4
turtledev <turtledev65@gmail.com>
2023-12-16 13:32:13 +0200

    Add h1 to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import UploadImages from "@/components/upload-images";
 export default function Home() {
   return (
     <div>
+      <h1 className="text-center text-5xl font-bold text-white">
+        Smash or Pass <br />
+        Maker
+      </h1>
       <UploadImages />
     </div>
   );
