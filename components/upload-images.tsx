27961a790493e97168d8b76b151beddd4686c859
turtledev <turtledev65@gmail.com>
2023-12-16 12:32:06 +0200

    Style upload dropzone

--- components/upload-images.tsx
+++ components/upload-images.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { BiImage, BiImages } from "react-icons/bi";
 
 import { useRouter } from "next/navigation";
 import { UploadDropzone } from "./util/uploadthing";
@@ -9,6 +10,7 @@ const UploadImages = () => {
 
   return (
     <UploadDropzone
+      className="ut-upload-icon:scale-120 border-none ut-button:mt-8 ut-button:bg-red-500 ut-allowed-content:text-xl ut-allowed-content:text-gray-300 ut-label:text-2xl ut-label:text-gray-100"
       endpoint="imageUploader"
       content={{
         label({ ready }) {
@@ -18,6 +20,11 @@ const UploadImages = () => {
         allowedContent({ ready, fileTypes }) {
           if (!ready) return "Loading...";
           return "max: 15";
+        },
+        uploadIcon() {
+          return (
+            <BiImages className="mx-auto block h-32 w-32 align-middle text-white" />
+          );
         }
       }}
       onClientUploadComplete={async res => {
